144510f05448cea5f879fd3e43eb2752f220f961
darklight9911 <muniaedu911@gmail.com>
2024-11-04 00:59:28 +0600

    Co-authored-by: Shagato <clickTwice26@users.noreply.github.com>

--- finsync.c
+++ finsync.c
@@ -1,6 +1,44 @@
-#include<stdio.h>
-int main(){
-    printf("hello");
-    printf("I edited this line");
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+/*
+Assignment 1:
+Title: USER INTERACTION
+Show 4 option. And let user to select them and then tell them what they selected.
+*/
+int main()
+{
+    while (1)
+    {
+
+        printf("[+]Select an option:\n[1]Create\n[2]View\n[3]Stats\n[4]Requests\n");
+        int input;
+        printf("Enter option number:\n");
+        scanf("%d", &input);
+        if (input == 1)
+        {
+            system("clear");
+            printf("Entered to the Creation.\n");
+        }
+        else if (input == 2)
+        {
+            system("clear");
+            printf("Entered to the view section.\n");
+        }
+        else if (input == 3)
+        {
+            system("clear");
+            printf("Entered to the statics.\n");
+        }
+        else if (input == 4)
+        {
+            system("clear");
+            printf("Entered to the Requests section.\n");
+        }
+        else
+        {
+            exit(0);
+        }
+    }
     return 0;
 }
